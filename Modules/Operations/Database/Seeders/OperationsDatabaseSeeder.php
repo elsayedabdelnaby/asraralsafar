cb8238a6ec078102bccb36bc86e621a71fd389b6
SCORPION <elsayed.fcsi.1@gmail.com>
2023-06-12 21:35:54 +0300

    remove un-use seeders & migrations

--- Modules/Operations/Database/Seeders/OperationsDatabaseSeeder.php
+++ Modules/Operations/Database/Seeders/OperationsDatabaseSeeder.php
@@ -14,6 +14,5 @@ class OperationsDatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(OperationsPermissionSeederTableSeeder::class);
-        $this->call(WorkingSettingsTableSeeder::class);
     }
 }
